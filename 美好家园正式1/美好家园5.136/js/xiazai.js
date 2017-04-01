b557e7935562bbf2b545f6665ae6acdcde8ea9bb
liyinglin <465514820@qq.com>
2017-04-01 09:36:11 +0800

    美好家园

---
+++ 美好家园正式1/美好家园5.136/js/xiazai.js
@@ -0,0 +1,17 @@
+/****点击下载****/
+$(function (){
+	$(".ly-as").on('touchstart mousedown',function(e){
+		$(".ly-as").attr("href","https://www.pgyer.com/D38W"); 
+	});
+	$('.cha').eq(0).on('touchstart mousedown',function(e){
+		$(this).parent().hide();
+	})
+	$(".ksweus").on("touchstart", "li .dj", function(event){
+	    location.href='https://www.pgyer.com/D38W';
+	});
+	$(".ksweus").on("touchstart", "li .imks", function(event){
+	    location.href='https://www.pgyer.com/D38W';
+	});
+})
+	
+
